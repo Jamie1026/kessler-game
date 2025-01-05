5fa8beda3980c45656beab955143d63ae34cbfb0
Jamie1026 <jamieyakimets@gmail.com>
2025-01-04 23:37:51 -0700

    made changes
    improved shooting ai
    added linear algebra and dimensionality

--- examples/jamie_controller.py
+++ examples/jamie_controller.py
@@ -3,6 +3,9 @@
 # NOTICE: This file is subject to the license agreement defined in file 'LICENSE', which is part of
 # this source code package.
 
+#things to work on:
+#mines, not reshooting target, not shooting at offscreen minus wrap,  
+
 from kesslergame import KesslerController
 from typing import Dict, Tuple
 import math
@@ -70,10 +73,11 @@ def prioritize_imminent_collision(ship_state: Dict, game_state: Dict) -> Dict:
     ship_pos = ship_state['position']
     ship_radius = ship_state['radius']
     map_size = game_state['map_size']
-    
+    bullet_speed = 800  # Speed of the ship's bullets (adjust as necessary)
+
     imminent_asteroid = None
     shortest_time = float('inf')
-    
+
     for asteroid in game_state['asteroids']:
         asteroid_pos = asteroid['position']
         asteroid_velocity = asteroid['velocity']
@@ -86,15 +90,43 @@ def prioritize_imminent_collision(ship_state: Dict, game_state: Dict) -> Dict:
         )
         if not (0 <= next_x <= map_size[0] and 0 <= next_y <= map_size[1]):
             continue  # Exclude asteroids imminently leaving the screen
-        
-        # Consider wrapping for collision time
-        t = time_to_collision_wrapped(ship_pos, ship_radius, asteroid_pos, asteroid_velocity, asteroid_radius, map_size)
-        
-        if t < shortest_time:
-            shortest_time = t
+
+        # Calculate collision time considering wrapping
+        t_col = time_to_collision_wrapped(ship_pos, ship_radius, asteroid_pos, asteroid_velocity, asteroid_radius, map_size)
+
+        # Future position of the target asteroid
+        ast_x, ast_y = asteroid['position']
+        ast_vx, ast_vy = asteroid['velocity']
+        t_int = 0
+        for _ in range(5):  # Refine prediction iteratively
+            future_ast_x = ast_x + ast_vx * t_int
+            future_ast_y = ast_y + ast_vy * t_int
+            dist_to_future_ast = math.sqrt((future_ast_x - ship_pos[0])**2 + (future_ast_y - ship_pos[1])**2)
+            t_int = dist_to_future_ast / bullet_speed
+
+        # Predict future asteroid position at collision time
+        future_ast_x = asteroid_pos[0] + asteroid_velocity[0] * t_int
+        future_ast_y = asteroid_pos[1] + asteroid_velocity[1] * t_int
+
+        # Check if the predicted future position is within screen bounds
+        if not (0 <= future_ast_x <= map_size[0] and 0 <= future_ast_y <= map_size[1]):
+            continue  # Skip asteroids that will be off-screen at collision time
+
+        # Verify that the bullet can realistically intercept the asteroid
+        dist_to_future_ast = math.sqrt((future_ast_x - ship_pos[0])**2 + (future_ast_y - ship_pos[1])**2)
+        bullet_travel_time = dist_to_future_ast / bullet_speed
+
+        # If the time to intercept (bullet) is significantly different from collision time, skip
+        #if abs(bullet_travel_time - t_col) > 0.1 * t_col:  # Allow a 10% tolerance
+        #    continue
+
+        # Update the closest imminent asteroid if this one is a better candidate
+        if t_col < shortest_time:
+            shortest_time = t_col
             imminent_asteroid = asteroid
-    
+
     return imminent_asteroid
+
 
 def time_to_collision(ship_pos, ship_radius, asteroid_pos, asteroid_velocity, asteroid_radius):
     x_s, y_s = ship_pos
@@ -159,49 +191,67 @@ class JamieController(KesslerController):
         Any variables or initialization desired for the controller can be set up here
         """
         ...
-
     def actions(self, ship_state: Dict, game_state: Dict) -> Tuple[float, float, bool, bool]:
-        # Find the closest asteroid
-        closest_ast = None
-        closest_ast_dist = float('inf')
-        for asteroid in game_state['asteroids']:
-            dist = math.sqrt((asteroid['position'][0] - ship_state['position'][0])**2 +
-                            (asteroid['position'][1] - ship_state['position'][1])**2)
-            if dist < closest_ast_dist:
-                closest_ast_dist = dist
-                closest_ast = asteroid
-        most_imminent_ast = prioritize_imminent_collision(ship_state, game_state)
-        if most_imminent_ast is not None:
-            closest_ast = most_imminent_ast
-        # Ship's position and heading
+        # Ship state and properties
         ship_x, ship_y = ship_state['position']
         ship_heading_deg = ship_state['heading']
-        bullet_speed = 800  # Approximate speed of the bullet
+        ship_heading_rad = math.radians(ship_heading_deg)
+        bullet_speed = 800  # Speed of bullets
+        
+        # Check for imminent collision
+        most_imminent_ast = prioritize_imminent_collision(ship_state, game_state)
+        if most_imminent_ast is not None:
+            print("IMMINENT")
+            we_gon_shot_this = most_imminent_ast
+        else:
+            print("CLOSEST")
+            # No imminent collision, find the asteroid requiring minimum rotation
+            min_rotation = float('inf')
+            we_gon_shot_this = None
 
-        # Asteroid's current position and velocity
-        ast_x, ast_y = closest_ast['position']
-        ast_vx, ast_vy = closest_ast['velocity']
+            for asteroid in game_state['asteroids']:
+                ast_x, ast_y = asteroid['position']
+                ast_vx, ast_vy = asteroid['velocity']
 
-        # Predict where the asteroid will be
-        t = 0  # Initial guess for time
-        for _ in range(5):  # Iterate to refine the prediction
+                # Predict the asteroid's future position
+                t = 0
+                for _ in range(5):  # Refine prediction iteratively
+                    future_ast_x = ast_x + ast_vx * t
+                    future_ast_y = ast_y + ast_vy * t
+                    dist_to_future_ast = math.sqrt((future_ast_x - ship_x)**2 + (future_ast_y - ship_y)**2)
+                    t = dist_to_future_ast / bullet_speed
+
+                # Calculate angle to the predicted position
+                angle_to_ast_rad = math.atan2(future_ast_y - ship_y, future_ast_x - ship_x)
+                angle_diff_rad = (angle_to_ast_rad - ship_heading_rad + math.pi) % (2 * math.pi) - math.pi
+                angle_diff_deg = abs(math.degrees(angle_diff_rad))
+
+                # Check if this asteroid requires less rotation
+                if angle_diff_deg < min_rotation:
+                    min_rotation = angle_diff_deg
+                    we_gon_shot_this = asteroid
+
+        # If no asteroids, don't fire
+        if we_gon_shot_this is None:
+            return 0, 0, False, False
+
+        # Future position of the target asteroid
+        ast_x, ast_y = we_gon_shot_this['position']
+        ast_vx, ast_vy = we_gon_shot_this['velocity']
+        t = 0
+        for _ in range(5):  # Refine prediction iteratively
             future_ast_x = ast_x + ast_vx * t
             future_ast_y = ast_y + ast_vy * t
             dist_to_future_ast = math.sqrt((future_ast_x - ship_x)**2 + (future_ast_y - ship_y)**2)
             t = dist_to_future_ast / bullet_speed
 
-        # Future position of the asteroid
         future_ast_x = ast_x + ast_vx * t
         future_ast_y = ast_y + ast_vy * t
+        print(future_ast_x, future_ast_y)
 
-        # Calculate angle to the predicted position
+        # Calculate angle to the target
         angle_to_ast_rad = math.atan2(future_ast_y - ship_y, future_ast_x - ship_x)
-        angle_to_ast_deg = math.degrees(angle_to_ast_rad)
-
-        # Calculate angle difference
-        ship_heading_rad = math.radians(ship_heading_deg)
-        angle_diff_rad = math.radians(angle_to_ast_deg) - ship_heading_rad
-        angle_diff_rad = (angle_diff_rad + math.pi) % (2 * math.pi) - math.pi  # Normalize to [-π, π]
+        angle_diff_rad = (angle_to_ast_rad - ship_heading_rad + math.pi) % (2 * math.pi) - math.pi
         angle_diff_deg = math.degrees(angle_diff_rad)
 
         # Set turn rate proportional to the angle difference
@@ -213,9 +263,10 @@ class JamieController(KesslerController):
         else:
             fire = False
         turn_rate = max(-max_turn_rate, min(turn_rate, max_turn_rate))
-
+        if ship_state["is_respawning"] :
+            fire = False
         # Set constant thrust
-        thrust = 50
+        thrust = 0
 
         # Always fire
         #fire = True
@@ -224,6 +275,7 @@ class JamieController(KesslerController):
         drop_mine = False
 
         return thrust, turn_rate, fire, drop_mine
+
 
     '''
     Goal: Aim at closest asteroid
@@ -245,3 +297,26 @@ class JamieController(KesslerController):
     # @property
     # def custom_sprite_path(self) -> str:
     #     return "Neo.png"
+
+'''
+Scenario 1:
+There is an asteroid going to hit us, and there is a closest asteroid
+
+Currently my code does:
+1) check all the asts 
+2) find ones about to hit us
+
+2A) Gettin hit -> Target ast hitting us 
+2B)) Nothing hitting - target closest one
+
+
+I want my code to:
+1) check all the asts 
+2) find ones about to hit us
+2A) Gettin hit -> Target ast hitting us 
+2B)) Nothing hitting - look for asteroid that can be shot with miniumum ship rotation required 
+
+
+'''
+
+print("Run scenario test, not this controller!")
--- examples/scenario_test.py
+++ examples/scenario_test.py
@@ -3,6 +3,9 @@
 # NOTICE: This file is subject to the license agreement defined in file 'LICENSE', which is part of
 # this source code package.
 
+
+
+
 import time
 
 from main_controller_akila import AkilaController
@@ -11,17 +14,100 @@ from test_controller import TestController
 from jamie_controller import JamieController
 from graphics_both import GraphicsBoth
 #from  kesslergame.controller_gamepad import GamepadController
-print('hello')
+from adversarial_scenarios_for_jie import *
+from custom_scenarios import *
+from xfc_2023_replica_scenarios import *
+
+xfc2023 = [
+     ex_adv_four_corners_pt1,
+     ex_adv_four_corners_pt2,
+     ex_adv_asteroids_down_up_pt1,
+     ex_adv_asteroids_down_up_pt2,
+     ex_adv_direct_facing,
+     ex_adv_two_asteroids_pt1,
+     ex_adv_two_asteroids_pt2,
+     ex_adv_ring_pt1,
+     adv_random_big_1,
+     adv_random_big_2,
+     adv_random_big_3,
+     adv_random_big_4,
+     adv_multi_wall_bottom_hard_1,
+     adv_multi_wall_right_hard_1,
+     adv_multi_ring_closing_left,
+     adv_multi_ring_closing_right,
+     adv_multi_two_rings_closing,
+     avg_multi_ring_closing_both2,
+     adv_multi_ring_closing_both_inside,
+     adv_multi_ring_closing_both_inside_fast
+ ]
+
+xfc2024 = [
+    adv_random_small_1,
+    adv_random_small_1_2,
+    adv_multi_wall_left_easy,
+    adv_multi_four_corners,
+    adv_multi_wall_top_easy,
+    adv_multi_2wall_closing,
+    adv_wall_bottom_staggered,
+    adv_multi_wall_right_hard,
+    adv_moving_corridor_angled_1,
+    adv_moving_corridor_angled_1_mines,
+    adv_multi_ring_closing_left,
+    adv_multi_ring_closing_left2,
+    adv_multi_ring_closing_both2,
+    adv_multi_ring_closing_both_inside_fast,
+    adv_multi_two_rings_closing
+]
+
+custom_scenarios = [
+    #target_priority_optimization1,
+    #closing_ring_scenario,
+    #easy_closing_ring_scenario,
+    #more_intense_closing_ring_scenario,
+    #rotating_square_scenario,
+    #rotating_square_2_overlap,
+    #falling_leaves_scenario,
+    #zigzag_motion_scenario,
+    #shearing_pattern_scenario,
+    #super_hard_wrap,
+    #wonky_ring,
+    #moving_ring_scenario,
+    #shifting_square_scenario,
+    #delayed_closing_ring_scenario,
+    #spiral_assault_scenario,
+    #dancing_ring,
+    #dancing_ring_2,
+    #intersecting_lines_scenario,
+    #exploding_grid_scenario,
+    #grid_formation_explosion_scenario,
+    #aspect_ratio_grid_formation_scenario,
+    #adv_asteroid_stealing,
+    #wrapping_nightmare,
+    #wrapping_nightmare_fast,
+    #purgatory,
+    #cross,
+    #fight_for_asteroid,
+    #shot_pred_test,
+    #shredder,
+    diagonal_shredder,
+    out_of_bound_mine,
+    explainability_1,
+    explainability_2,
+    split_forecasting,
+    minefield_maze_scenario,
+    wrap_collision_test
+]
+
 # Define game scenario
 my_test_scenario = Scenario(name='Test Scenario',
-                            #num_asteroids=5,
-                            asteroid_states=[
-                                {'position': (800, 400), 'angle': 0, 'speed': 200, 'size': 4},
-                                {'position': (800, 100), 'angle': 0, 'speed': 200, 'size': 4}
-                            ],
+                            num_asteroids=5,
+                            #asteroid_states=[
+                            #    {'position': (800, 400), 'angle': 0, 'speed': 200, 'size': 4},
+                            #    {'position': (800, 100), 'angle': 0, 'speed': 200, 'size': 4}
+                            #],
                             ship_states=[
                                 {'position': (300, 400), 'angle': 90, 'lives': 3, 'team': 1, "mines_remaining": 3},
-                                #{'position': (400, 600), 'angle': 90, 'lives': 3, 'team': 2, "mines_remaining": 3},
+                                {'position': (400, 600), 'angle': 90, 'lives': 3, 'team': 2, "mines_remaining": 3},
                             ],
                             map_size=(1000, 700),
                             time_limit=60,
@@ -40,7 +126,10 @@ game = KesslerGame(settings=game_settings)  # Use this to visualize the game sce
 
 # Evaluate the game
 pre = time.perf_counter()
-score, perf_data = game.run(scenario=my_test_scenario, controllers=[JamieController(), AkilaController()])
+
+
+for sc in xfc2024 :
+    score, perf_data = game.run(scenario=sc, controllers=[JamieController(), AkilaController()])
 
 # Print out some general info about the result
 print('Scenario eval time: '+str(time.perf_counter()-pre))
